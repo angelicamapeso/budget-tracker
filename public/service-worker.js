60a33dc3c03e952d2b2c9327afcce62efa65815a
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-12-14 20:07:38 -0500

    Add fetch event listener for service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -43,3 +43,40 @@ self.addEventListener("activate", function (event) {
     })
   );
 });
+
+// fetch event listener:
+// GET /api/transaction - return cached data on fail
+// any other fetch - return any matching static cache or necessary response
+self.addEventListener("fetch", function (event) {
+  if (
+    event.request.method === "GET" &&
+    event.request.url.includes("/api/transaction")
+  ) {
+    event.respondWith(
+      caches.open(DATA_CACHE).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            if (response.status === 200) {
+              cache.put(event.request, response.clone());
+              return response;
+            }
+          })
+          .catch(err => {
+            return cache.match(event.request).then(response => {
+              return response;
+            });
+          });
+      })
+    );
+
+    return;
+  }
+
+  event.respondWith(
+    caches.open(STATIC_CACHE).then(cache => {
+      return cache.match(event.request).then(response => {
+        return response || fetch(event.request);
+      });
+    })
+  );
+});
