dd6e5553f1e2a11840fffedcbfb6353ab54f67c8
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-12-14 19:30:07 -0500

    Add install listener on service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,26 @@
-console.log("Hello! It's your service worker!");
+const STATIC_CACHE = "static-cache-v1";
+const DATA_CACHE = "data-cache-v1";
+const FILES_TO_CACHE = [
+  "/",
+  "/index.js",
+  "/indexedDB.js",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+];
+
+// install service worker:
+// 1. cache static files
+// 2. cache data data
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    Promise.all([
+      caches.open(STATIC_CACHE).then(function (cache) {
+        return cache.addAll(FILES_TO_CACHE);
+      }),
+      caches.open(DATA_CACHE).then(function (cache) {
+        return cache.add("/api/transaction");
+      }),
+    ])
+  );
+});
