d03f8050433e1b131e64f265baf671d0d551cfd4
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-12-15 17:22:11 -0500

    Export sendTransaction func

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -5,17 +5,27 @@ import {
 } from "./indexedDB.js";
 import { getTransactions, postTransaction } from "./api.js";
 import { populateTotal, populateTable, populateChart } from "./populate.js";
+import { sendTransaction } from "./form.js";
 
 let transactions = [];
 let myChart;
 
 window.addEventListener("load", function () {
+  // Register service worker
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("/service-worker.js");
   }
 
+  // Add event listeners
+  document.querySelector("#add-btn").onclick = function () {
+    sendTransaction(true, transactions, myChart);
+  };
+  document.querySelector("#sub-btn").onclick = function () {
+    sendTransaction(false, transactions, myChart);
+  };
   window.addEventListener("online", postIndexedTransactions);
 
+  // Get data and load page
   getTransactions().then(data => {
     // save db data on global variable
     transactions = data;
@@ -40,65 +50,3 @@ window.addEventListener("load", function () {
     }
   });
 });
-
-function sendTransaction(isAdding) {
-  let nameEl = document.querySelector("#t-name");
-  let amountEl = document.querySelector("#t-amount");
-  let errorEl = document.querySelector(".form .error");
-
-  // validate form
-  if (nameEl.value === "" || amountEl.value === "") {
-    errorEl.textContent = "Missing Information";
-    return;
-  } else {
-    errorEl.textContent = "";
-  }
-
-  // create record
-  let transaction = {
-    name: nameEl.value,
-    value: amountEl.value,
-    date: new Date().toISOString(),
-  };
-
-  // if subtracting funds, convert amount to negative number
-  if (!isAdding) {
-    transaction.value *= -1;
-  }
-
-  // add to beginning of current array of data
-  transactions.unshift(transaction);
-
-  // re-run logic to populate ui with new record
-  populateChart(transactions, myChart);
-  populateTable(transactions);
-  populateTotal(transactions);
-
-  // also send to server
-  postTransaction(transaction)
-    .then(data => {
-      if (data.errors) {
-        errorEl.textContent = "Missing Information";
-      } else {
-        // clear form
-        nameEl.value = "";
-        amountEl.value = "";
-      }
-    })
-    .catch(err => {
-      // fetch failed, so save in indexed db
-      indexTransaction(transaction);
-
-      // clear form
-      nameEl.value = "";
-      amountEl.value = "";
-    });
-}
-
-document.querySelector("#add-btn").onclick = function () {
-  sendTransaction(true);
-};
-
-document.querySelector("#sub-btn").onclick = function () {
-  sendTransaction(false);
-};
